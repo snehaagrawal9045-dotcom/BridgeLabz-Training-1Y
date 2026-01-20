d4f6c25ff1ee372457f7da45ddc9f65d065b5655
snehaagrawal9045-dotcom <snehaagrawal9045@gmail.com>
2026-01-20 20:02:52 +0530

    Added subtraction program in Workshop branch

---
+++ Java-codes/Subtraction.java
@@ -0,0 +1,12 @@
+public class Subtraction{
+    public static void main(Strings[] args)
+{	   
+	   int a=5;
+	   int b=6;
+	    
+	   int c = a+b;
+		
+		
+	   System.out.println("Addition = " + c);
+	}
+}
