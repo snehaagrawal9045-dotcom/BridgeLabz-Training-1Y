60166c684ac768a8562a4c802f8f6e46ebaa0ed4
snehaagrawal9045-dotcom <snehaagrawal9045@gmail.com>
2026-01-20 19:55:25 +0530

    Added addition program in Workshop branch

---
+++ Java-codes/Addition.java
@@ -0,0 +1,12 @@
+public class Addition{
+    public static void main(Strings[] args)
+{	   
+	   int a=5;
+	   int b=6;
+	    
+	   int sum = a+b;
+		
+		
+	   System.out.println("Addition = " + sum);
+	}
+}
