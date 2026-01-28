bf1d5099d1dab1476f07175ed9d386c26b63539c
snehaagrawal9045-dotcom <snehaagrawal9045@gmail.com>
2026-01-28 09:43:36 +0530

    Added prigram in Workshop

---
+++ Java-codes/Counter.java
@@ -0,0 +1,11 @@
+public class Counter {
+public static void main(String[] args) {
+// create the counter variable
+int counter = 0;
+// print the counter
+while (counter < 20) {
+counter = counter + 1;
+System.out.println("Counter = " + counter);
+}
+}
+}
