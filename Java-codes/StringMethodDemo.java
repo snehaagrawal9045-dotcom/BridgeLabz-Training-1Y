66b4b620f418a7edf4c797434dbe8826fc71fd7f
snehaagrawal9045-dotcom <snehaagrawal9045@gmail.com>
2026-02-06 15:08:48 +0530

    Added in Workshop

---
+++ Java-codes/StringMethodDemo.java
@@ -0,0 +1,16 @@
+class StringMethodDemo{
+public static void main(String[] args){
+
+String s = "Java Programming";
+
+System.out.println(s.length());
+System.out.println(s.toUpperCase());
+System.out.println(s.toLowerCase());
+System.out.println(s.charAt(5));
+System.out.println(s.substring(5));
+System.out.println(s.substring(0,4));
+System.out.println(s.contains("Java"));
+System.out.println(s.equals("Java Programming"));
+}
+}
+
