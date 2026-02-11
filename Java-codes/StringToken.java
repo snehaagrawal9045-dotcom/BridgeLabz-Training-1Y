3b40de0283ca5098f82933f4e40fca9ba323cddf
snehaagrawal9045-dotcom <snehaagrawal9045@gmail.com>
2026-02-11 09:29:29 +0530

    Added in Workshop

---
+++ Java-codes/StringToken.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class StringToken{
+
+static int a =20;
+static int b =40;
+
+static int addition (int a , int b )
+{
+return a+b;
+}
+public static void main(String[] args) {
+
+int result = addition(10,20);
+System.out.println(result);
+System.out.println(b);
+}
+}
