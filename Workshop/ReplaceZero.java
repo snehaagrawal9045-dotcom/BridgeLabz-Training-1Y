658b8069afb57728bf2a9491546944477e40afef
snehaagrawal9045-dotcom <snehaagrawal9045@gmail.com>
2026-04-13 15:19:57 +0530

    Update ReplaceZero.java1

--- Workshop/ReplaceZero.java
+++ Workshop/ReplaceZero.java
@@ -4,7 +4,7 @@ public class ReplaceZero {
     public static void main(String[] args){
         int arr[] = {1, 2, 3, 2, 5};
 
-        int target = 2; // jisko replace karna h
+        int target = 1;
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
